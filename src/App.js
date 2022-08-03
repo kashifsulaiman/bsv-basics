e0f31ce86375b36d6c68616f329a6dedf68d2d34
Mohammad Kashif Sulaiman <m.kqagraphix@gmail.com>
2022-08-03 18:58:28 +0500

    feat: add bsv keys, addresses & QRCode

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import * as bsv from 'bsv'
+import CryptoJS from 'crypto-js'
+import { QRCodeSVG } from 'qrcode.react'
 
 function App() {
+  const [privateKey, setPrivateKey] = useState()
+  const [encKey, setEncKey] = useState()
+  const [decKey, setDecKey] = useState()
+  const [publicKey, setPublicKey] = useState()
+  const [address, setAddress] = useState()
+
+  useEffect(() => {
+    generateKeys()
+  }, [])
+
+  const generateKeys = () => {
+    const privateKey = bsv.PrivKey.fromRandom()
+    const publicKey = bsv.PubKey.fromPrivKey(privateKey)
+    const address = bsv.Address.fromPubKey(publicKey)
+
+    const password = 'test-123'
+    const ciphertext = CryptoJS.AES.encrypt(privateKey.toString(), password).toString()
+    const bytes  = CryptoJS.AES.decrypt(ciphertext, password);
+    const originalText = bytes.toString(CryptoJS.enc.Utf8);
+
+    setPrivateKey(privateKey.toString())
+    setEncKey(ciphertext)
+    setDecKey(originalText)
+    setPublicKey(publicKey.toString())
+    setAddress(address.toString())
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Private Key: {privateKey}</h3>
+      <h3>Encrypted Key: {encKey}</h3>
+      <h3>Decrypted Key: {decKey}</h3>
+      <h3>Public Key: {publicKey}</h3>
+      <h3>Address: {address}</h3>
+      <QRCodeSVG value={`bitcoinsv: ${address}`} />
     </div>
   );
 }
